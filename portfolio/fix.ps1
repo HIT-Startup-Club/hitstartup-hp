b0c16dfb889076fe48a2c0e05dc89498de0fcab2
nekopath <bm22137@cc.it-hiroshima.ac.jp>
2026-01-30 19:24:53 +0900

    第１公開前　色々変えた

--- portfolio/fix.ps1
+++
@@ -1,8 +0,0 @@
-﻿ = Get-ChildItem -Path  = [char]0x5E83 + [char]0x5CF6 + [char]0x5DE5 + [char]0x696D + [char]0x5927 + [char]0x5B66 + ' ' + [char]0x8D77 + [char]0x696D + [char]0x90E8
- in  = [System.IO.File]::ReadAllText( = New-Object System.Text.UTF8Encoding  = '<span class="font-bold text-lg tracking-tight">[^<]+</span>'
-     + '</span>'
-    , )
-    
-    [System.IO.File]::WriteAllText(, .Name)
-}
-Write-Host 'Done'
