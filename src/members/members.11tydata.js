1308326835f43bd8ae3805b98f88c999ad2ae55c
nekopath <bm22137@cc.it-hiroshima.ac.jp>
2026-02-04 18:18:48 +0900

    portfolioからmembersに変更

--- src/members/members.11tydata.js
+++ src/members/members.11tydata.js
@@ -2,8 +2,8 @@ module.exports = {
   layout: "portfolio-member.njk",
   tags: ["member"],
   eleventyComputed: {
-    permalink: data => data.eleventyExcludeFromCollections ? false : `portfolio/${data.slug}.html`,
-    title: data => data.eleventyExcludeFromCollections ? "" : `${data.name} - Portfolio | HIT Startup Club`,
+    permalink: data => data.eleventyExcludeFromCollections ? false : `members/${data.slug}.html`,
+    title: data => data.eleventyExcludeFromCollections ? "" : `${data.name} | HIT Startup Club`,
     copyrightName: data => data.eleventyExcludeFromCollections ? "" : data.nameEn
   }
 };
